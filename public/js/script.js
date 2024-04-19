771792e9c2818c1d187566534314ce7b441bb346
mhdfrhan <mhdfarhan5148@gmail.com>
2024-04-19 09:44:45 +0700

    update: 19-04-2024

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,28 @@
+// document.body.style.overflow = "hidden";
+
+// window.onload = function () {
+//   var loaderWrapper = document.querySelector(".loader-wrapper");
+//   loaderWrapper.style.opacity = "0";
+//   loaderWrapper.style.visibility = "hidden";
+//   loaderWrapper.style.transition = "visibility 0s 0.5s, opacity 0.5s linear";
+
+//   document.body.style.overflow = "visible";
+// };
+
+AOS.init({
+  once: true,
+  duration: 800,
+});
+
+window.addEventListener("scroll", function () {
+  var banner = document.getElementById("banner");
+  if (window.scrollY > 50) {
+    banner.classList.add("scrolled");
+  } else {
+    banner.classList.remove("scrolled");
+  }
+});
+
 function animateIfVisible() {
   const elements = document.querySelectorAll(
     ".font-cabinet-grotesk[data-count]"
@@ -50,4 +75,3 @@ document.addEventListener("DOMContentLoaded", animateIfVisible);
 
 window.addEventListener("scroll", animateIfVisible);
 window.addEventListener("resize", animateIfVisible);
-
